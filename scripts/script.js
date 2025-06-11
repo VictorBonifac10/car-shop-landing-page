c2078ede4939e3ab1620da072dd8f65812c42cd6
victorbonifac10 <victor.alvesbonifacio@gmail.com>
2025-06-10 21:20:49 -0300

    Finalizacao dos estilos e script

--- scripts/script.js
+++ scripts/script.js
@@ -4,23 +4,33 @@ let container = document.querySelector('.container')
 let items = container.querySelectorAll('.list .item')
 let indicator = document.querySelector('.indicators')
 let dots = indicator.querySelectorAll('ul li')
+let list = container.querySelector('.list')
 
 let active = 0
 let firstposition = 0
 let lastPosition = items.length - 1
 
-nextButton.onclick = () => {
+function setSlider() {
     let itemOld = container.querySelector('.list .item.active')
     itemOld.classList.remove('active')
 
+    let dotsOld = indicator.querySelector('ul li.active')
+    dotsOld.classList.remove('active')
+    dots[active].classList.add('active')
+
+    indicator.querySelector('.number').innerHTML = '0' + (active + 1)
+}
+
+nextButton.onclick = () => {
+    list.style.setProperty('--calculation', 1)
     active = active + 1 > lastPosition ? 0 : active + 1
+    setSlider()
     items[active].classList.add('active')
 }
 
 prevButton.onclick = () => { //revisar
-    let itemOld = container.querySelector('.list .item.active')
-    itemOld.classList.remove('active')
-
-    active = active - 1 < lastPosition ? 0 : active - 1
+    list.style.setProperty('--calculation', -1)
+    active = active - 1 < firstposition ? lastPosition : active - 1
+    setSlider()
     items[active].classList.add('active')
 }
